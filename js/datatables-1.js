ab431b16c5a80afb05f6784a86627fb4532ad77d
dp3why <dp3why@gmail.com>
2023-11-22 07:59:43 -0800

    update table page

---
+++ js/datatables-1.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  fetch("./data/EV_Population.csv")
+    .then((response) => response.text())
+    .then((csvData) => {
+      const rows = csvData.split("\n").map((row) => row.split(","));
+
+      const table = new simpleDatatables.DataTable("#datatablesSimple", {
+        data: {
+          headings: rows[0], // Use the first row as headings
+          data: rows.slice(1, 500), // Exclude the first row for data
+        },
+        searchable: true,
+        perPage: 50,
+      });
+    })
+    .catch((error) => console.error("Error fetching or parsing CSV:", error));
+});
